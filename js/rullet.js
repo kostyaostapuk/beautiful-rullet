13d185b9735dab55d582fa192a552667ae3c9d63
Kostya Ostapuk <ostapuk97@gmail.com>
2017-12-24 20:56:39 +0300

    fix positions and function random positions slots

--- js/rullet.js
+++ js/rullet.js
@@ -11,32 +11,32 @@ $(document).ready(function() {
 
   //Start position
   var slot1_pos,
-	 		slot2_pos,
-			slot3_pos;
-	slot1_pos=slot2_pos=slot3_pos=0;
+    slot2_pos,
+    slot3_pos;
+  slot1_pos = slot2_pos = slot3_pos = 0;
 
   //Start speed slots
   var slot1_time,
-	 		slot2_time,
-			slot3_time;
-	slot1_time=slot2_time=slot3_time=80;
+    slot2_time,
+    slot3_time;
+  slot1_time = slot2_time = slot3_time = 80;
 
   var slot1_intrvl,
-			slot2_intrvl,
-      slot3_intrvl;
+    slot2_intrvl,
+    slot3_intrvl;
 
-	var posArr=[
-		0, //number 7
-		108, //bigwin
-		216, //cherry
-		325, //banana
-		435, //watermelon
-		540, //orange
-		651, //plum
-		760, //lemon
-		866, //bar
-	];
-	var toggle=false;
+  var posArr = [
+    0, //number 7
+    108, //bigwin
+    216, //cherry
+    325, //banana
+    435, //watermelon
+    540, //orange
+    651, //plum
+    760, //lemon
+    866, //bar
+  ];
+  var toggle = false;
 
   //Start spin
   $("#start").click(function() {
@@ -46,7 +46,7 @@ $(document).ready(function() {
         slot1_pos += 150;
         slot1.css("background-position", "0 " + slot1_pos + "px");
       }, slot1_time);
-    }, 600); //500
+    }, 600);
 
     var slot2_TO = setTimeout(function() {
       slot2.css("background-image", "url(../img/blur.png)");
@@ -54,7 +54,7 @@ $(document).ready(function() {
         slot2_pos += 150;
         slot2.css("background-position", "0 " + slot2_pos + "px");
       }, slot2_time);
-    }, 700); //600
+    }, 700);
 
     var slot3_TO = setTimeout(function() {
       slot3.css("background-image", "url(../img/blur.png)");
@@ -62,60 +62,41 @@ $(document).ready(function() {
         slot3_pos += 150;
         slot3.css("background-position", "0 " + slot3_pos + "px");
       }, slot3_time);
-    }, 800); //700
+    }, 800);
 
-		//SlideUp Slots
-		var slideUp1=setInterval(function () {
-			slot1.addClass("slideUp");
-		},200);
-		var slideUp2=setInterval(function () {
-			slot2.addClass("slideUp");
-		},400);
-		var slideUp3=setInterval(function () {
-			slot3.addClass("slideUp");
-		},600);
-		setTimeout(function(){	//Stop SlideUp
-			clearInterval(slideUp1,slideUp2, slideUp3);
-		}, 610);
-
-
-
-		setTimeout(stopRullet, 3000);
+    //SlideUp Slots
+    var slideUp1 = setInterval(function() {
+      slot1.addClass("slideUp");
+    }, 200);
+    var slideUp2 = setInterval(function() {
+      slot2.addClass("slideUp");
+    }, 400);
+    var slideUp3 = setInterval(function() {
+      slot3.addClass("slideUp");
+    }, 600);
+    setTimeout(function() { //Stop SlideUp
+      clearInterval(slideUp1, slideUp2, slideUp3);
+    }, 610);
+    setTimeout(stopRullet, 3000);
   });
 
   function stopRullet() {
     clearInterval(slot1_intrvl);
     clearInterval(slot2_intrvl);
     clearInterval(slot3_intrvl);
-    slot1_pos = 0;
-    slot2_pos = 0;
-    slot3_pos = 0;
-    $(".slot").css("background-position", "0 -" + 0 + "px");
+
+
     $(".slot").css("background-image", "url(../img/default.png)");
-		toggle=true;
-		
-		if (toggle==true) {
-			//SlideDown Slots
-			var slideDown1=setInterval(function () {
-				slot1.addClass("slideDown");
-			},200);
-			var slideDown2=setInterval(function () {
-				slot2.addClass("slideDown");
-			},400);
-			var slideDown3=setInterval(function () {
-				slot3.addClass("slideDown");
-			},600);
-			setTimeout(function(){	//Stop SlideUp
-				clearInterval(slideDown1,slideDown2, slideDown3);
-				$(".slot").css("background-position", "0 -" + 0 + "px");
-			}, 610);
-		}
+    slot1_pos = posArr[randomPosition()];
+    slot2_pos = posArr[randomPosition()];
+    slot3_pos = posArr[randomPosition()];
+    slot1.css("background-position", "0 -" + slot1_pos + "px");
+    slot2.css("background-position", "0 -" + slot2_pos + "px");
+    slot3.css("background-position", "0 -" + slot3_pos + "px");
+  }
 
-}
-
-
-
-
-
-
+  function randomPosition() {
+    var rand = Math.floor(Math.random() * posArr.length);
+    return rand;
+  }
 });
